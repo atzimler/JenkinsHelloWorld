68fe114ecfcf6f65456865b5da49344cf8fb32ae
Attila Tamas Zimler <attila.tamas.zimler@icloud.com>
2017-03-04 15:53:32 +1100

    Adding NUnitHelloWorld test project.

---
+++ NUnitHelloWorld/TestFixture.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace NUnitHelloWorld
+{
+    [TestFixture]
+    public class TestFixture
+    {
+        [Test]
+        public void ThisTestShouldPass()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [Test]
+        public void ThisTestShouldFail()
+        {
+            Assert.IsTrue(false);
+        }
+    }
+}
